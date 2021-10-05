613a750756bcdd214511b7c57ddf46da60d8c4a4
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-10-06 00:34:34 +0300

    added raw array

--- cilk/tests/test_raw_array.cpp
+++ cilk/tests/test_raw_array.cpp
@@ -2,6 +2,17 @@
 #include "raw_array.h"
 #include <cstdint>
 
+struct test_struct
+{
+    int32_t value;
+
+    test_struct(test_struct&& other) : value(other.value)
+    {
+        other.value = -1;
+    }
+};
+
+
 TEST(raw_array, simple_test)
 {
     raw_array<int32_t> arr(10);
@@ -14,3 +25,14 @@ TEST(raw_array, simple_test)
         ASSERT_EQ(arr[i], i + 10);
     }
 }
+
+TEST(raw_array, test_moving_values)
+{
+    raw_array<test_struct> arr(10);
+    test_struct x;
+    x.value = 10;
+    arr[0] = std::move(x);
+    ASSERT_EQ(arr[0].value, 10);
+    ASSERT_EQ(x.value, -1);
+}
+
