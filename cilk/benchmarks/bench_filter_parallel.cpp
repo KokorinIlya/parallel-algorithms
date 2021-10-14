b4ea7613102c947cc1f9f47d8d76dc00ce4eb9e7
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-10-15 00:09:47 +0300

    added filter

--- cilk/benchmarks/bench_filter_parallel.cpp
+++ cilk/benchmarks/bench_filter_parallel.cpp
@@ -1,28 +1,35 @@
-#include "map_parallel.h"
+#include "filter_parallel.h"
 #include <chrono>
 #include <random>
 #include <iostream>
 #include <cstdint>
+#include <vector>
+#include <functional>
 
-int32_t inc(int32_t const& x)
+template <typename T>
+std::vector<T> filter_sequential(raw_array<T> const& vals, std::function<bool(T const&)> pred)
 {
-    return x + 1;
-}
-
-raw_array<int32_t> map_sequential(raw_array<int32_t> const& from, std::function<int32_t(int32_t const&)> mapper)
-{
-    raw_array<int32_t> result(from.get_size());
-    for (uint32_t i = 0; i < from.get_size(); ++i)
+    std::vector<T> res;
+    assert(vals.is_valid() && vals.get_size() > 0);
+    for (uint32_t i = 0; i < vals.get_size(); ++i)
     {
-        result[i] = mapper(from[i]);
+        if (pred(vals[i]))
+        {
+            res.push_back(vals[i]);
+        }
     }
-    return result;
+    return res;
 }
 
 uint64_t measure(std::default_random_engine& generator, std::uniform_int_distribution<int32_t>& elements_distribution,
-                uint32_t sz, uint32_t blocks_count, uint32_t reps)
+                uint32_t sz, uint32_t blocks_count, uint32_t reps, int32_t divisor)
 {
     uint64_t sum = 0;
+    std::function<bool(int32_t const&)> pred = [divisor](int32_t const& x)
+    {
+        return x % divisor == 0;
+    }
+
     for (uint32_t i = 0; i < reps; ++i)
     {
         raw_array<int32_t> arr(sz);
@@ -33,11 +40,11 @@ uint64_t measure(std::default_random_engine& generator, std::uniform_int_distrib
         std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
         if (blocks_count == 0)
         {
-           map_sequential(arr, &inc);
+           filter_sequential(arr, pred);
         }
         else
         {
-            map_parallel<int32_t, int32_t>(arr, &inc, blocks_count);
+            filter_parallel<int32_t>(arr, pred, blocks_count);
         }
         std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
         sum += std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
@@ -52,12 +59,12 @@ int main()
     uint32_t sz = 10000000;
     uint32_t reps = 10;
 
-    uint64_t res = measure(generator, elements_distribution, sz, 0, reps);
+    uint64_t res = measure(generator, elements_distribution, sz, 0, reps, 5);
     std::cout << "Sequential, elapsed " << res << " milliseconds" << std::endl;
 
-    for (uint32_t i = 10; i <= 160; i += 10)
+    for (uint32_t i = 1; i <= 16; ++i)
     {
-        uint64_t res = measure(generator, elements_distribution, sz, i, reps);
+        uint64_t res = measure(generator, elements_distribution, sz, i * 10, reps, 5);
         std::cout << i * 10 << " blocks, elapsed " << res << " milliseconds" << std::endl;
     }
     return 0;
--- cilk/benchmarks/bench_scan.cpp
+++ cilk/benchmarks/bench_scan.cpp
@@ -42,7 +42,7 @@ int main()
 
     for (uint32_t i = 10; i <= 160; i += 10)
     {
-        uint64_t res = measure(generator, elements_distribution, sz, i * 10, reps);
+        uint64_t res = measure(generator, elements_distribution, sz, i, reps);
         std::cout << i << " blocks, elapsed " << res << " milliseconds" << std::endl;
     }
     return 0;
