5d247afa2a79365bf47b8803eb9d9c5fc93851d3
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-10-16 00:20:01 +0300

    sort tests added

--- cilk/src/include/sort_parallel.h
+++ cilk/src/include/sort_parallel.h
@@ -16,7 +16,7 @@
 #include <ctime>
 
 template <typename T>
-void copy_parallel(raw_array<T> const& src, raw_array<T>& dst, uint32_t start_idx, uint32_t blocks_count)
+void copy_parallel(raw_array<T> const& src, raw_array<T>& dst, uint32_t start_idx, uint32_t seq_block_size)
 {
     assert(src.get_size() + start_idx <= dst.get_size());
     if (src.get_size() == 0)
@@ -24,20 +24,16 @@ void copy_parallel(raw_array<T> const& src, raw_array<T>& dst, uint32_t start_id
         return;
     }
 
-    if (blocks_count > src.get_size())
+    uint32_t blocks_count = src.get_size() / seq_block_size;
+    if (src.get_size() % seq_block_size != 0)
     {
-        blocks_count = src.get_size();
-    }
-    uint32_t elements_per_block = src.get_size() / blocks_count;
-    if (src.get_size() % blocks_count != 0)
-    {
-        ++elements_per_block;
+        ++blocks_count;
     }
     #pragma grainsize 1
     cilk_for (uint32_t i = 0; i < blocks_count; ++i)
     {
-        uint32_t left = i * elements_per_block;
-        uint32_t right = left + elements_per_block;
+        uint32_t left = i * seq_block_size;
+        uint32_t right = left + seq_block_size;
         if (right > src.get_size())
         {
             right = src.get_size();
@@ -49,15 +45,14 @@ void copy_parallel(raw_array<T> const& src, raw_array<T>& dst, uint32_t start_id
     }
 }
 
-// TODO: maybe, try seq_block_size instead of blocks_count
 template <typename T>
-void do_sort_parallel(raw_array<T>& arr, uint32_t seq_sort_threshold, uint32_t blocks_count)
+void do_sort_parallel(raw_array<T>& arr, uint32_t seq_block_size)
 {
     if (arr.get_size() == 0)
     {
         return;
     }
-    if (arr.get_size() <= seq_sort_threshold)
+    if (arr.get_size() <= seq_block_size)
     {
         T* data_ptr = arr.get_raw_ptr();
         std::sort(data_ptr, data_ptr + arr.get_size());
@@ -66,28 +61,30 @@ void do_sort_parallel(raw_array<T>& arr, uint32_t seq_sort_threshold, uint32_t b
     uint32_t partitioner_idx = static_cast<uint32_t>(std::rand()) % arr.get_size();
     T const& partitioner = arr[partitioner_idx];
 
+    uint32_t blocks_count = arr.get_size() / seq_block_size;
+
     raw_array<T> le = cilk_spawn filter_parallel<T>(arr, [&partitioner](T const& x) { return x <  partitioner; }, blocks_count);
     raw_array<T> eq = cilk_spawn filter_parallel<T>(arr, [&partitioner](T const& x) { return x == partitioner; }, blocks_count);
     raw_array<T> gt =            filter_parallel<T>(arr, [&partitioner](T const& x) { return x >  partitioner; }, blocks_count);
     cilk_sync;
 
-    cilk_spawn do_sort_parallel(le, seq_sort_threshold, blocks_count);
-    cilk_spawn do_sort_parallel(eq, seq_sort_threshold, blocks_count);
-               do_sort_parallel(gt, seq_sort_threshold, blocks_count);
+    cilk_spawn do_sort_parallel(le, seq_block_size);
+    cilk_spawn do_sort_parallel(eq, seq_block_size);
+               do_sort_parallel(gt, seq_block_size);
     cilk_sync;
 
-    cilk_spawn copy_parallel(le, arr, 0,                             blocks_count);
-    cilk_spawn copy_parallel(eq, arr, le.get_size(),                 blocks_count);
-               copy_parallel(gt, arr, le.get_size() + eq.get_size(), blocks_count);
+    cilk_spawn copy_parallel(le, arr, 0,                             seq_block_size);
+    cilk_spawn copy_parallel(eq, arr, le.get_size(),                 seq_block_size);
+               copy_parallel(gt, arr, le.get_size() + eq.get_size(), seq_block_size);
     cilk_sync;
 }
 
 
 template <typename T>
-void sort_parallel(raw_array<T>& arr, uint32_t seq_sort_threshold, uint32_t blocks_count)
+void sort_parallel(raw_array<T>& arr, uint32_t seq_block_size)
 {
     std::srand(time(0)); // TODO: maybe, try C++-style random
-    do_sort_parallel(arr, seq_sort_threshold, blocks_count);
+    do_sort_parallel(arr, seq_block_size);
 }
 
 #endif
