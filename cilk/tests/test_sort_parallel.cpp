888f00e9221dd59af08fa3b26f3700fa3e2f478d
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-10-16 00:20:34 +0300

    sort tests added

--- cilk/tests/test_sort_parallel.cpp
+++ cilk/tests/test_sort_parallel.cpp
@@ -12,7 +12,7 @@ TEST(parallel_sort, simple)
     {
         arr[i] = v[i];
     }
-    sort_parallel(arr, 1, 3);
+    sort_parallel<int32_t>(arr, 3);
     std::vector<int32_t> exp_res({-8, -2, 1, 2, 3, 3, 4, 5, 6, 7});
     for (uint32_t i = 0; i < exp_res.size(); ++i)
     {
