1478eab58267429ada46c19967e70c0ca97bbc15
Ilya Kokorin <kokorin.ilya.1998@gmail.com>
2021-12-05 15:51:19 +0000

    removed taken

--- cilk/src/include/bfs.h
+++ cilk/src/include/bfs.h
@@ -52,15 +52,14 @@ inline std::vector<int64_t> bfs_sequential(
 inline void process_single_node(
     std::unordered_map<uint64_t, std::vector<uint64_t>> const& edges,
     pasl::pctl::parray<int64_t> const& cur_frontier,
-    pasl::pctl::parray<std::atomic<bool>>& taken,
-    pasl::pctl::parray<int64_t>& result,
+    pasl::pctl::parray<std::atomic<int64_t>>& result,
     pasl::pctl::parray<int64_t>& new_frontier,
     pasl::pctl::parray<uint64_t> const& pref_sizes,
     uint64_t node_idx)
 {
     assert(cur_frontier[node_idx] >= 0);
     uint64_t from_node = cur_frontier[node_idx];
-    assert(taken[from_node].load() && result[from_node] >= 0);
+    assert(result[from_node].load() >= 0);
     uint64_t start_idx = pref_sizes[node_idx];
 
     auto it = edges.find(from_node);
@@ -70,21 +69,20 @@ inline void process_single_node(
         assert(to_nodes.size() > 0);
         pasl::pctl::parallel_for(
             static_cast<uint64_t>(0), static_cast<uint64_t>(to_nodes.size()),
-            [&taken, &result, &new_frontier, &to_nodes, start_idx, from_node](uint64_t edge_idx)
+            [&result, &new_frontier, &to_nodes, start_idx, from_node](uint64_t edge_idx)
             {
                 assert(new_frontier[start_idx + edge_idx] == -1);
                 uint64_t to_node = to_nodes[edge_idx];
-                bool expected_taken = false;
-                if (taken[to_node].compare_exchange_strong(expected_taken, true))
+                int64_t expected_result = -1;
+                int64_t new_result = result[from_node].load() + 1;
+                if (result[to_node].compare_exchange_strong(expected_result, new_result))
                 {
-                    assert(expected_taken == false);
-                    assert(result[to_node] == -1);
-                    result[to_node] = result[from_node] + 1;
+                    assert(expected_result == -1);
                     new_frontier[start_idx + edge_idx] = to_node;
                 }
                 else
                 {
-                    assert(expected_taken == true);
+                    assert(expected_result >= 0);
                 }
             }
         );
@@ -96,16 +94,14 @@ inline pasl::pctl::parray<int64_t> bfs_cas(
     std::unordered_map<uint64_t, std::vector<uint64_t>> const& edges)
 {
     assert(0 <= start_node && start_node < nodes_count);
-    pasl::pctl::parray<int64_t> result(nodes_count, static_cast<int64_t>(-1)); // TODO: parray<atomic<int64_t>>
-    result[start_node] = 0;
-    pasl::pctl::parray<std::atomic<bool>> taken(
+    pasl::pctl::parray<std::atomic<int64_t>> result(
         nodes_count,
-        [](long) -> bool
+        [](long) -> int64_t
         {
-            return false;
+            return static_cast<int64_t>(-1);
         } 
     );
-    taken[start_node].store(true);
+    result[start_node].store(0);
     pasl::pctl::parray<int64_t> cur_frontier = {static_cast<int64_t>(start_node)};
 
     while (true)
@@ -114,11 +110,11 @@ inline pasl::pctl::parray<int64_t> bfs_cas(
 
         pasl::pctl::parray<uint64_t> sizes(
             cur_frontier.size(),
-            [&cur_frontier, &edges, &taken, &result](uint64_t idx)
+            [&cur_frontier, &edges, &result](uint64_t idx)
             {
                 assert(cur_frontier[idx] >= 0);
                 uint64_t cur_node = static_cast<uint64_t>(cur_frontier[idx]);
-                assert(taken[cur_node].load() && result[cur_node] >= 0);
+                assert(result[cur_node].load() >= 0);
 
                 auto it = edges.find(cur_node);
                 if (it != edges.end())
@@ -150,7 +146,7 @@ inline pasl::pctl::parray<int64_t> bfs_cas(
         uint64_t new_frontier_size = pref_sizes[pref_sizes.size() - 1] + sizes[sizes.size() - 1];
         pasl::pctl::parray<int64_t> new_frontier(new_frontier_size, static_cast<int64_t>(-1));
 
-        pasl::pctl::range::parallel_for(
+        pasl::pctl::range::parallel_for( // TODO: use ordinary for
             static_cast<uint64_t>(0), static_cast<uint64_t>(cur_frontier.size()),
             [&pref_sizes, new_frontier_size](uint64_t left, uint64_t right)
             {
@@ -163,16 +159,16 @@ inline pasl::pctl::parray<int64_t> bfs_cas(
                 }
                 return end_size - pref_sizes[left];
             },
-            [&edges, &cur_frontier, &taken, &result, &new_frontier, &pref_sizes](uint64_t node_idx)
+            [&edges, &cur_frontier, &result, &new_frontier, &pref_sizes](uint64_t node_idx)
             {
-                process_single_node(edges, cur_frontier, taken, result, new_frontier, pref_sizes, node_idx);
+                process_single_node(edges, cur_frontier, result, new_frontier, pref_sizes, node_idx);
             },
-            [&edges, &cur_frontier, &taken, &result, &new_frontier, &pref_sizes](uint64_t left, uint64_t right)
+            [&edges, &cur_frontier, &result, &new_frontier, &pref_sizes](uint64_t left, uint64_t right)
             {
                 assert(0 <= left && left < right && right <= pref_sizes.size());
                 for (uint64_t i = left; i < right; ++i)
                 {
-                    process_single_node(edges, cur_frontier, taken, result, new_frontier, pref_sizes, i);
+                    process_single_node(edges, cur_frontier, result, new_frontier, pref_sizes, i);
                 }
             }
         );
@@ -187,7 +183,13 @@ inline pasl::pctl::parray<int64_t> bfs_cas(
         );
         if (cur_frontier.size() == 0)
         {
-            return result;
+            return pasl::pctl::parray<int64_t>( // TODO: return result
+                nodes_count,
+                [&result](uint64_t idx) -> int64_t
+                {
+                    return result[idx].load();
+                }
+            );
         }
     }
 }
